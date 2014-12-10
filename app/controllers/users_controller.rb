deac0f07d92f52aa07da99c9f2e0fc50831e640c
chinmaydd <chinmay1dd@gmail.com>
2014-12-10 20:24:24 +0530

    Added show for Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def create
     @user = User.new(user_param)
