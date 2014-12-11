d4aee101fe35464afbe51abeb702321bc4b99961
chinmaydd <chinmay1dd@gmail.com>
2014-12-11 18:32:02 +0530

    Added UserInfo validation

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'spring',        group: :development
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.7'
 
+ gem 'validates_email_format_of'
+ #Gem to validate email ids
+
 # Use unicorn as the app server
 # gem 'unicorn'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
 	has_many :posts
 	has_secure_password
+
+	validates :email, email_format: { message: "This does not look like a valid email address" }  #Using gem 'validates_email_format_of'
+	validates :password, length: { in: 6..20 }
+	validates :first_name, length: { in: 2..15}
+	validates :last_name, length: { in: 2..15}
+	validates :username, format: { with: /\w/ }, uniqueness: true
 end
